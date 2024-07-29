f93b1bb26119f427ee2d2fd51f29f881036e3563
Francesco Caracciolo <67018178+FrancescoCaracciolo@users.noreply.github.com>
2024-07-29 13:23:19 +0200

    Setup for pypi

--- src/livepng/__init__.py
+++ src/livepng/__init__.py
@@ -1,1 +1,4 @@
-VERSION = 1
+from livepng.model import LivePNG
+from livepng.objects import Style, Expression, Variant
+from livepng.inspector import ModelInspector
+from livepng.validator import ModelValidator
